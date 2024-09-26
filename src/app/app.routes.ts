15574d4eaf7b0ff4f46f9836c54414bc5831db15
raulfaz <refaz@espe.edu.ec>
2024-09-26 08:22:00 -0500

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { EmpresaComponent } from './empresa/empresa.component';
 import { ProductosComponent } from './productos/productos.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'inicio', pathMatch: 'full'},
     {path: 'inicio', component:InicioComponent},
     {path: 'empresa', component:EmpresaComponent},
     {path: 'productos', component:ProductosComponent}
