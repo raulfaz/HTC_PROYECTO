a7eeeb90e0dc8dc0ad904992fd6646330cfd758a
raulfaz <refaz@espe.edu.ec>
2024-09-27 11:57:43 -0500

    día 5

--- src/app/form-cotizacion/form-cotizacion.component.ts
+++ src/app/form-cotizacion/form-cotizacion.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-cotizacion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './form-cotizacion.component.html',
   styleUrl: './form-cotizacion.component.css'
 })
 export class FormCotizacionComponent {
-
+  
 }
