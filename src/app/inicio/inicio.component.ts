e2fc8d84bb6931c02b4bd28363471caf6c08c39e
MelKaCaicedo <108081802+MelKaCaicedo@users.noreply.github.com>
2024-09-27 10:00:13 -0500

    Reapply "día 5"
    This reverts commit a4515de0738cc7051247991efb3226d126116c16.

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CarrosulComponent } from '../carrosul/carrosul.component';
 import { RouterOutlet } from '@angular/router';
+import { SERVICIOS } from '../datos/servicios';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CarrosulComponent],
+  imports: [CarrosulComponent,CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
@@ -13,4 +15,16 @@ export class InicioComponent {
   imagenUno: string = '../../assets/img-carrosul/uno.jpg';
   imagenDos: string = '../../assets/img-carrosul/dos.jpg';
   imagenTres: string ='../../assets/img-carrosul/tres.jpg';
+
+  activeIndex = 0;
+
+  nextService() {
+    this.activeIndex = (this.activeIndex + 1) % this.servicios.length;
+  }
+
+  prevService() {
+    this.activeIndex = (this.activeIndex - 1 + this.servicios.length) % this.servicios.length;
+  }
+
+  servicios = SERVICIOS
 }
