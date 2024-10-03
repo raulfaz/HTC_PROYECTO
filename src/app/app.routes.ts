38ee6e8c55bd14066d9b6715d39906444f8289bb
MelKaCaicedo <108081802+MelKaCaicedo@users.noreply.github.com>
2024-10-03 09:50:41 -0500

    día 8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { EmpresaComponent } from './empresa/empresa.component';
 import { ProductosComponent } from './productos/productos.component';
+import { CotizacionComponent } from './cotizacion/cotizacion.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
     {path: 'inicio', component:InicioComponent},
     {path: 'empresa', component:EmpresaComponent},
-    {path: 'productos', component:ProductosComponent}
-
+    {path: 'productos', component:ProductosComponent},
+    {path: 'cotizacion', component:CotizacionComponent}
 ];
--- src/app/form-cotizacion/form-cotizacion.component.ts
+++ src/app/form-cotizacion/form-cotizacion.component.ts
@@ -5,10 +5,20 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-form-cotizacion',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './form-cotizacion.component.html',
   styleUrl: './form-cotizacion.component.css'
 })
 export class FormCotizacionComponent {
-  
+  nombre: string = '';
+  email: string = '';
+  telefono: string = '';
+  mensaje: string = '';
+
+  onSubmit() {
+    if (this.nombre && this.email && this.telefono && this.mensaje) {
+      // Acción al enviar el formulario
+      console.log('Formulario enviado:', this.nombre, this.email, this.telefono, this.mensaje);
+    }
+  }
 }
