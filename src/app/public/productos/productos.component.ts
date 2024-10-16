9f01f5131bb7fea9715c3123e8b1858106bc0899
MelKaCaicedo <108081802+MelKaCaicedo@users.noreply.github.com>
2024-10-16 09:00:32 -0500

    nose

--- src/app/public/productos/productos.component.ts
+++ src/app/public/productos/productos.component.ts
@@ -1,71 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SERVICIOS } from '../datos/servicios';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatIconModule],
+  imports: [CommonModule, FormsModule, MatIconModule, MatPaginatorModule],
   templateUrl: './productos.component.html',
-  styleUrl: './productos.component.css'
+  styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  servicios = SERVICIOS
-
+  servicios = SERVICIOS;
   serviciosFiltrados = [...this.servicios];
   busqueda: string = '';
-
   currentPosition = 0;
-  itemWidth = 200; // Ancho de cada catálogo (ajustar según necesidad)
+  itemWidth = 200;
 
   catalogData = [
     { name: 'Catálogo 1', image: '/../assets/catalogs/catalog1.jpg', pdf: 'catalog1.pdf' },
     { name: 'Catálogo 2', image: '/../assets/catalogs/catalog2.jpeg', pdf: 'catalog2.pdf' },
     { name: 'Catálogo 3', image: '/../assets/catalogs/catalog3.jpg', pdf: 'catalog3.pdf' }
-    // Puedes agregar más catálogos aquí
   ];
 
-  // Método para abrir el catálogo en PDF
   openCatalog(pdf: string) {
     window.open(`/../assets/catalogs/${pdf}`, '_blank');
   }
 
-  // Desplazamiento hacia la izquierda
   prevCatalog() {
     if (this.currentPosition === 0) {
-      this.currentPosition = -(this.itemWidth * (this.catalogData.length - 1)); // Vuelve al último
+      this.currentPosition = -(this.itemWidth * (this.catalogData.length - 1));
     } else {
       this.currentPosition += this.itemWidth;
     }
   }
 
-  // Desplazamiento hacia la derecha
   nextCatalog() {
     if (this.currentPosition <= -(this.itemWidth * (this.catalogData.length - 1))) {
-      this.currentPosition = 0; // Vuelve al primero
+      this.currentPosition = 0;
     } else {
       this.currentPosition -= this.itemWidth;
     }
   }
 
-  // Método para buscar productos
   buscarProductos() {
     const busquedaNormalizada = this.busqueda
       .toLowerCase()
-      .normalize('NFD') // Normaliza la cadena
-      .replace(/[\u0300-\u036f]/g, ''); // Elimina las tildes
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
 
     this.serviciosFiltrados = this.servicios.filter(servicio => {
       const servicioNombreNormalizado = servicio.name
         .toLowerCase()
         .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, ''); // Normaliza el nombre del servicio
+        .replace(/[\u0300-\u036f]/g, '');
       return servicioNombreNormalizado.includes(busquedaNormalizada);
     });
+    this.paginator.pageIndex = 0; // Reinicia el paginador al primer índice
   }
 
   categorias: string[] = [
@@ -81,12 +77,14 @@ export class ProductosComponent {
     'Mantenimiento'
   ];
 
-  // Filtra los servicios según la categoría seleccionada
   filtrarPorCategoria(categoria: string) {
-    // Implementa la lógica para filtrar servicios
-    console.log(`Filtrando por la categoría: ${categoria}`);
-    // Ejemplo: this.serviciosFiltrados = this.servicios.filter(servicio => servicio.categoria === categoria);
+    // this.serviciosFiltrados = this.servicios.filter(servicio => servicio.categoria === categoria);
+    this.paginator.pageIndex = 0; // Reinicia el paginador al primer índice
   }
 
-
+  actualizarPaginador() {
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+    const endIndex = startIndex + this.paginator.pageSize;
+    this.serviciosFiltrados = this.servicios.slice(startIndex, endIndex);
+  }
 }
