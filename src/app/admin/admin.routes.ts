184cd1f111e585b0652e92b97890b222311ef69e
raulfaz <refaz@espe.edu.ec>
2024-10-16 08:28:06 -0500

    .

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from "@angular/router";
 import { PrincipalComponent } from "./principal/principal.component";
+import { ProductosComponent } from "./productos/productos.component";
+import { CatalogosComponent } from "./catalogos/catalogos.component";
+import { PerfilUsuarioComponent } from "./perfil-usuario/perfil-usuario.component";
 
 export const admin: Routes = [
     {
         path: '',
         component: PrincipalComponent,
         children: [
-            {path: '', redirectTo: 'principal', pathMatch: 'full'},
-            {path: 'principal', component: PrincipalComponent}
+            {path: '', redirectTo: 'productos', pathMatch: 'full'},
+           // {path: 'principal', component: PrincipalComponent},
+            {path: 'productos', component: ProductosComponent},
+            {path: 'catalogos', component: CatalogosComponent},
+            {path: 'perfil-usuario', component: PerfilUsuarioComponent}
         ]
     }
 ];
